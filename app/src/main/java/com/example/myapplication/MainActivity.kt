f7d44d145855c78cc6db033830f3f6956179f056
Willa Cai <contact@willacai.com>
2023-03-04 11:05:29 -0800

    Chapter 2: bindings, added next button

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,24 +1,35 @@
 package com.example.myapplication
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+import layout.Question
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+    private var currentIndex: Int = 0
+
+    private val questionBank = listOf(
+        Question(R.string.q_australia, true),
+        Question(R.string.q_oceans, true),
+        Question(R.string.q_africa, true),
+        Question(R.string.q_mideast, true),
+        Question(R.string.q_asia, false)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // inflates activity_main view, instantiates vars defined there
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        // setContentView(R.layout.activity_main) // inflates activity_main view, instantiates vars defined there
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton.setOnClickListener { view: View ->
+        binding.trueButton.setOnClickListener { view: View ->
             // trigger a toast
             Toast.makeText(
                 this, // gets add'l info about where the toast is showing
@@ -27,7 +38,7 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
 
-        falseButton.setOnClickListener {
+        binding.falseButton.setOnClickListener {
             Toast.makeText(
                 this, // gets add'l info about where the toast is showing
                 R.string.incorrect_toast,
@@ -35,6 +46,17 @@ class MainActivity : AppCompatActivity() {
             ).show()
         }
 
-        // start the countdown timer
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size // wraparound
+            updateQuestion()
+        }
+
+        updateQuestion()
+
+    }
+
+    private fun updateQuestion(){
+        val questionTextResId =  questionBank[currentIndex].textResId // get reference
+        binding.questionTextView.setText(questionTextResId) // use reference
     }
 }
